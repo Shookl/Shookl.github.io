cec1432927d8b88244c922f6aae3735a6588e777
Shookl <dsp_ne_fanera@mail.ru>
2021-05-26 19:18:42 +0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,1 @@
+"use strict";var e=document.querySelector(".header-mobile__btn_js"),o=document.querySelector(".header-mobile_js"),t=document.querySelector(".header-mobile__close_js");e.addEventListener("click",function(){o.classList.add("header-mobile_open")}),t.addEventListener("click",function(){o.classList.remove("header-mobile_open")});
